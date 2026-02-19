30711ed06575d3a4436b13e1fa0d25c08e574e1f
adharsh-codes <adharshpavan5@gmail.com>
2026-02-18 20:52:50 -0800

    Time: 0 ms (100.00%) | Memory: 43 MB (48.61%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String toLowerCase(String s) {
+        /*//for(int i=0;i<s.length();i++){
+          //  char ch=s.charAt(i);
+        //}
+        for(char ch:s.toCharArray()){
+            char lower=Character.toLowerCase(ch);
+        }*/
+        String lower=s.toLowerCase();
+        return lower;
+    }
+}
